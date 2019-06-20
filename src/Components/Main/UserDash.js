1d30f74817667d64d589cb949d1343b7bf6b1c77
David Ren <dren391@gmail.com>
2019-06-20 12:34:59 -0400

    styling

--- src/Components/Main/UserDash.js
+++ src/Components/Main/UserDash.js
@@ -1,4 +1,6 @@
 import React from "react";
+// import { Button } from "react-bootstrap";
+import { Card, Icon, Image, Button } from 'semantic-ui-react';
 
 class UserDash extends React.Component {
 
@@ -15,30 +17,40 @@ class UserDash extends React.Component {
 
     render() {
         return (
-            <div>
-							<h3>My Dashboard</h3>
-							<div>Book a lesson
-								<div id="monday-div">
-									<span id="monday-span">Monday 9:00am</span>
-									<button onClick={this.mondayHandler}>Book</button>
-								</div>
-								<div>
-									<span>Tuesday 11:30am</span>
-									<button onClick={this.bookHandler}>Book</button>
-								</div>
-								<div>
-									<span>Wednesday 2:00pm</span>
-									<button onClick={this.bookHandler}>Book</button>
-								</div>
-							</div>
-							<br></br>
-
-								<ul>My Appointments
-									<li>Thursday 3:45pm</li>
-									<li>{this.state.clickElement}</li>
-								</ul>
-	
-            </div>
+				<Card.Group>
+					<Card style={{ marginLeft: '2em' }}>
+						<Image src='https://i.imgur.com/UIB6yuO.png' wrapped ui={false} />
+						<Card.Content>
+							<Card.Header>Ethan</Card.Header>
+							<Card.Meta>
+								<span className='date'>Joined in 2015</span>
+							</Card.Meta>
+							<Card.Description>
+								Canon Genius
+							</Card.Description>
+						</Card.Content>
+						<Card.Content extra>
+							<Button>Book</Button>
+							<Button>Info</Button>							
+						</Card.Content>
+  				</Card>
+							<Card style={{ marginLeft: '2em' }}>
+							<Image src='https://i.imgur.com/rh3jQjo.png' wrapped ui={false} />
+							<Card.Content>
+								<Card.Header>Robert</Card.Header>
+								<Card.Meta>
+									<span className='date'>Joined in 2017</span>
+								</Card.Meta>
+								<Card.Description>
+									Canon Genius
+								</Card.Description>
+							</Card.Content>
+							<Card.Content extra>
+								<Button>Book</Button>
+								<Button>Info</Button>							
+							</Card.Content>
+						</Card>
+				</Card.Group>
         )
     }
 }
@@ -55,4 +67,33 @@ export default UserDash;
 		// 		instructor_id: 1,
 				
 		// 	})
-    // })//end of fetch 
+		// })//end of fetch 
+		
+		       // <div>
+						// 	<h3>My Dashboard</h3>
+						// 	<h4>Book a Lesson</h4>
+						// 	<div>
+						// 		<div id="monday-div">
+						// 			<span id="monday-span">Monday 9:00am</span>
+						// 			<button onClick={this.mondayHandler}>Book</button>
+						// 			<Button className="mr-2" variant="primary" href="/signup">
+            //   			Book
+            // 			</Button>
+						// 		</div>
+						// 		<div>
+						// 			<span>Tuesday 11:30am</span>
+						// 			<button onClick={this.bookHandler}>Book</button>
+						// 		</div>
+						// 		<div>
+						// 			<span>Wednesday 2:00pm</span>
+						// 			<button onClick={this.bookHandler}>Book</button>
+						// 		</div>
+						// 	</div>
+						// 	<br></br>
+						// 		<h3>My Appointments</h3>
+						// 		<ul>
+						// 			<li>Thursday 3:45pm</li>
+						// 			<li>{this.state.clickElement}</li>
+						// 		</ul>
+	
+            // </div>
